602ddfacce72ad0e83d7138c55b3aa2cc1a0c4ab
Aro Andriamaro <ni.aro.aina@gmail.com>
2021-05-16 19:40:57 +0200

    Add few components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+// components
+import Home from './containers/Home';
+import About from './containers/About';
 
 export default function App() {
   return (
     <Router>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+      </nav>
       <Switch>
         <Route exact path='/'>
-          <h1>Hello world</h1>
+          <Home />
+        </Route>
+        <Route exact path='/about'>
+          <About />
         </Route>
       </Switch>
     </Router>
---
+++ src/containers/About.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export default function Home() {
+  return <h1>About Page</h1>;
+}
